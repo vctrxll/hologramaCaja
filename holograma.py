a166e080e0eef68995d488d5a8a019a9f8dd2f47
Victor Rodriguez <viictor.axel11@gmail.com>
2024-11-09 18:19:24 -0600

    arreglo de coordenadas

--- holograma.py
+++ holograma.py
@@ -69,13 +69,14 @@ makeoptimize = isoptimized == "SI"  # Convierte la cadena en una variable boolea
 cap_width = 640
 cap_height = 360
 logo = True  # Bandera para indicar si se está mostrando el logo.
+isFullscreen = True
 
 
 # Cargar el modelo 3D
 mesh = o3d.io.read_triangle_mesh(objectreadfile, True)  # Lee el modelo 3D desde el archivo.
 # Crear la ventana de visualización del modelo 3D
 vis = o3d.visualization.Visualizer()  # Crea un visualizador de Open3D.
-vis.create_window(window_name="Open3D", width=1920, height=1080)
+vis.create_window(window_name="Open3D", width=960, height=540)
 vis.add_geometry(mesh)  # Añade el modelo 3D a la ventana de visualización.
 vis.get_render_option().load_from_json(json_path)  # Carga las opciones de renderización desde un archivo JSON.
 vis.get_view_control().set_zoom(0.7)  # Establece el nivel de zoom de la vista.
@@ -83,9 +84,12 @@ vis.get_view_control().rotate(300, 1000, xo=0.0, yo=0.0)  # Rota la vista del mo
 vis.get_view_control().rotate(1000, 0, xo=0.0, yo=0.0)  # Rota la vista del modelo 3D.
 vis.poll_events()  # Procesa los eventos de la ventana.
 vis.update_renderer()  # Actualiza el renderizador.
-hwnd = win32gui.FindWindow(None, 'Open3D')
-win32gui.SetWindowLong(hwnd, win32con.GWL_STYLE, win32con.WS_POPUP | win32con.WS_VISIBLE)
-win32gui.SetWindowPos(hwnd, win32con.HWND_TOP, 0, 0, win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1), win32con.SWP_FRAMECHANGED)
+
+if isFullscreen:
+    hwnd = win32gui.FindWindow(None, 'Open3D')
+    win32gui.SetWindowLong(hwnd, win32con.GWL_STYLE, win32con.WS_POPUP | win32con.WS_VISIBLE)
+    win32gui.SetWindowPos(hwnd, win32con.HWND_TOP, 0, 0, win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1),
+                          win32con.SWP_FRAMECHANGED)
 
 print("Ejecutando...")  # Imprime un mensaje indicando que el programa está en ejecución.
 print(mp.__file__)
