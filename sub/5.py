51ab0b6b196bc329150366bc26e80b65825deb87
Victor Rodriguez <viictor.axel11@gmail.com>
2025-02-24 19:08:08 -0600

    sub 5.py

--- sub/5.py
+++ sub/5.py
@@ -19,23 +19,29 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = "50,770"  # Ajusta las coordenadas según n
 screen = create_subtitle_window()
 
 
-audio_path = "../../AUDIOS/ESPANOL/8E.mp3"
+audio_path = "../AUDIOS/2025/5.mp3"
 audio = pygame.mixer.Sound(audio_path)
 audio_length = audio.get_length()
 
 # Diccionario con subtítulos sincronizados
-dialogue =  {
-        0: "Aquí presentamos una fascinante vasija efigie antropomorfa...",
-        5: "... representando a un personaje asociado a un chinanteco Pichinche.",
-        10: "Un antiguo poblador de lo que se conocía como la Chinantla Pichinche...",
-        15: "... cercano al actual pueblo de Yolox.",
-        20: "Podemos identificar esta particularidad gracias al peinado del personaje...",
-        25: "... una especie de trenzado que recorre toda su cabeza.",
-        30: "El personaje representado parece estar muerto, con los ojos entrecerrados...",
-        35: "... simbolizando la última imagen del mundo terrenal.",
-        40: "Su boca abierta sugiere que su espíritu se está escapando...",
-        45: "... reflejando la visión y las creencias sobre la muerte y el más allá en la cultura chinanteca."
-    }
+dialogue = {
+    0: "OLLA BAJA POLICROMA TRÍPODE",
+    3: "Esta es la séptima parada de nuestro recorrido.",
+    10: "Aquí destacamos la Olla Baja Policroma Trípode,",
+    13: "una vasija fina elaborada en arcilla de tonalidad crema y de grosor delicado.",
+    15: "Sus características, en la pasta, forma y decoración,",
+    20: "la convierten en una pieza diagnóstica de la región chinanteca.",
+    24: "Su ornamentación está vinculada a rituales ancestrales y simbolismos profundos.",
+    30: "La presencia de una serpiente fantástica invita a relacionarla con Quetzalcóatl,",
+    38: "importante divinidad mesoamericana, que simboliza fertilidad, creación y el orden del cosmos.",
+    44: "En el borde, se aprecia una banda solar simplificada",
+    48: "con representaciones de rayos y espinas de sacrificio en tonalidades rojas y negras.",
+    54: "Cada detalle revela un mensaje sagrado",
+    56: "y la profunda espiritualidad de culturas antiguas,",
+    59: "invitándonos a reflexionar sobre el misticismo y la historia de estos pueblos.",
+    65: "Esta pieza enriquece nuestra colección",
+    69: "y conecta nuestro presente con el eco de tiempos sagrados."
+}
 
 start_time = time.time()
 running = True
